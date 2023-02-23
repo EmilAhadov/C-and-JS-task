d141e2683de8c56bc863209a6a43b9ffb6770ae6
madPhysix <froggyrain123@gmail.com>
2023-02-23 18:17:21 +0400

    Folder created

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+string name = "John";
+int integer = 123;
+float number1 = 123.3f;
+double number2 = 123.462d;
+decimal number3 = 123.43723642m;
+bool sss = true;
+long number4 = 2135372;
+Console.WriteLine(number4);
+
+
---
+++ New.js
@@ -0,0 +1,6 @@
+var string = "SMT"
+var number = 12
+var boolean = true
+var symbol = "S"
+var bigint = 124164124
+console.log(string)
